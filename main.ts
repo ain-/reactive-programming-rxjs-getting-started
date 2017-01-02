7ffd66b423dd49f4ae8307b1f67d36cec8d9164c
Ain <ain.salula@gmail.com>
2017-01-02 13:38:02 +0200

    using RxJS operators

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ let source = Observable.create(observer => {
         observer.next(numbers[index++]);
 
         if(index < numbers.length) {
-            setTimeout(produceValue, 2000);
+            setTimeout(produceValue, 250);
         }
         else {
             observer.complete();
@@ -16,7 +16,8 @@ let source = Observable.create(observer => {
     }
 
     produceValue();
-});
+}).map(n => n * 2)
+  .filter(n => n > 4);
 
 source.subscribe(
     value => console.log(`value: ${value}`),
