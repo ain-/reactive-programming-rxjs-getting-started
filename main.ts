ecc29ed159bf7829978f88ce363bbde63a4b697f
Ain <ain.salula@gmail.com>
2017-01-02 13:13:58 +0200

    creating your first observable and observer

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-alert("ok");
+import {Observable, Observer} from 'rxjs';
+
+let numbers = [1, 5, 10]
+let source = Observable.from(numbers);
+
+class MyObserver implements Observer<number> {
+
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log("complete");
+    }
+}
+
+source.subscribe(new MyObserver());
+source.subscribe(new MyObserver());
