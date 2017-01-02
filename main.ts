b9343697aa8c6cfeed83f39879a02ffa4c7d4755
Ain <ain.salula@gmail.com>
2017-01-02 13:23:21 +0200

    using observable.create

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 import {Observable, Observer} from 'rxjs';
 
 let numbers = [1, 5, 10]
-let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+    for(let n of numbers) {
+        if(n === 5) {
+            observer.error("Something went wrong!");
+        }
+        observer.next(n);
+    }
+
+    observer.complete();
+});
 
 source.subscribe(
     value => console.log(`value: ${value}`),
