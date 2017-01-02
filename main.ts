a270f568750f4487fbaf0bf39f799a02ec07b365
Ain <ain.salula@gmail.com>
2017-01-02 15:01:27 +0200

    Sending Requests with XmlHttpRequest

--- main.ts
+++ main.ts
@@ -1,24 +1,28 @@
 import {Observable} from 'rxjs';
 
-let circle = document.getElementById("circle");
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-let source = Observable.fromEvent(document, "mousemove")
-    .map((e : MouseEvent) => {
-        return {
-            x: e.clientX,
-            y: e.clientY
-        }
-    })
-    .filter(value => value.x < 500)
-    .delay(300);
+let click = Observable.fromEvent(document, "click");
 
-function onNext(value) {
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        });
+    });
+
+    xhr.open("GET", url);
+    xhr.send()
 }
 
-source.subscribe(
-    onNext,
+click.subscribe(
+    e => load("movies.json"),
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
