5c4ff0f67954524d9d22bad0848099deb9928314
Ain <ain.salula@gmail.com>
2017-01-02 15:55:43 +0200

    implementing retry logic with retry and retryWhen

--- main.ts
+++ main.ts
@@ -10,14 +10,30 @@ function load(url: string) {
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener("load", () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if(xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
         });
 
         xhr.open("GET", url);
         xhr.send()
-    });
+    }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
+}
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return function(errors) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
 }
 
 function renderMovies(movies) {
@@ -28,7 +44,7 @@ function renderMovies(movies) {
     });
 }
 
-load("movies.json").subscribe(renderMovies);
+//load("movies.json").subscribe(renderMovies);
 
 click.flatMap(e => load("movies.json"))
     .subscribe(
