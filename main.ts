9334b4e4489e82441f09da5b0d3d28078795542d
Ain <ain.salula@gmail.com>
2017-01-02 15:19:02 +0200

    using flatMap to process inner observables

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
@@ -6,23 +6,33 @@ let button = document.getElementById("button");
 let click = Observable.fromEvent(document, "click");
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
         });
-    });
 
-    xhr.open("GET", url);
-    xhr.send()
+        xhr.open("GET", url);
+        xhr.send()
+    });
 }
 
-click.subscribe(
-    e => load("movies.json"),
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+load("movies.json").subscribe(renderMovies);
+
+click.flatMap(e => load("movies.json"))
+    .subscribe(
+    renderMovies,
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
