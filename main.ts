1a78002724d0e81f310ac354657d140f1021216b
Ain <ain.salula@gmail.com>
2017-01-02 13:24:38 +0200

    cleanup

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-import {Observable, Observer} from 'rxjs';
+import {Observable} from 'rxjs';
 
 let numbers = [1, 5, 10]
 let source = Observable.create(observer => {
     for(let n of numbers) {
-        if(n === 5) {
-            observer.error("Something went wrong!");
-        }
         observer.next(n);
     }
 
@@ -17,18 +14,3 @@ source.subscribe(
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
-
-class MyObserver implements Observer<number> {
-
-    next(value) {
-        console.log(`value: ${value}`);
-    }
-
-    error(e) {
-        console.log(`error: ${e}`);
-    }
-
-    complete() {
-        console.log("complete");
-    }
-}
