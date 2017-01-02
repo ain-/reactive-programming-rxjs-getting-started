6cc16fcffdad8734d2ddd6081e449f20dca04c94
Ain <ain.salula@gmail.com>
2017-01-02 13:59:16 +0200

    processing mouse events

--- main.ts
+++ main.ts
@@ -1,31 +1,24 @@
 import {Observable} from 'rxjs';
 
-//import {Observable} from "rxjs/Observable";
-//import "rxjs/add/operator/map";
-//import "rxjs/add/operator/filter";
+let circle = document.getElementById("circle");
 
-
-let numbers = [1, 5, 10]
-let source = Observable.create(observer => {
-    
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
-
-        if(index < numbers.length) {
-            setTimeout(produceValue, 250);
+let source = Observable.fromEvent(document, "mousemove")
+    .map((e : MouseEvent) => {
+        return {
+            x: e.clientX,
+            y: e.clientY
         }
-        else {
-            observer.complete();
-        }
-    }
+    })
+    .filter(value => value.x < 500)
+    .delay(300);
 
-    produceValue();
-}).map(n => n * 2)
-  .filter(n => n > 4);
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
 
 source.subscribe(
-    value => console.log(`value: ${value}`),
+    onNext,
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
