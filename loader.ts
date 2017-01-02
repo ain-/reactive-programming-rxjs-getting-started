0c12e2104cabacbe134554054b9e911463563282
Ain <ain.salula@gmail.com>
2017-01-02 17:08:03 +0200

    revisiting the retryWhen strategy

--- loader.ts
+++ loader.ts
@@ -21,18 +21,27 @@ export function load(url: string) {
 
  export function loadWithFetch(url: string) {
     return Observable.defer(() => {
-        return Observable.fromPromise(fetch(url).then(r => r.json()));
-    });
+        return Observable.fromPromise(fetch(url).then(r => {
+            if(r.status === 200) {
+                return r.json()
+            } else {
+                return Promise.reject(r);
+            }
+        }));
+    }).retryWhen(retryStrategy());
 }
 
-function retryStrategy({attempts = 4, delay = 1000}) {
+function retryStrategy({attempts = 4, delay = 1000} = {}) {
     return function(errors) {
         return errors
             .scan((acc, value) => {
-                console.log(acc, value);
-                return acc + 1;
+                acc += 1;
+                if (acc < attempts) {
+                    return acc;
+                } else {
+                    throw new Error(value);
+                }
             }, 0)
-            .takeWhile(acc => acc < attempts)
             .delay(delay);
     }
 }
